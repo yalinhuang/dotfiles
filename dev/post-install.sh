9decccd493584af0440fd8dd11533ad1917c689e
Ya-Lin Huang <yalinh@netflix.com>
2018-10-03 13:38:50 -0700

    vimrc and vim-polygot

--- dev/post-install.sh
+++ dev/post-install.sh
@@ -1,2 +1,6 @@
 for java in $(find /Library/Java/JavaVirtualMachines -mindepth 1 -maxdepth 1 ! -type l); do yes | (jenv add "$java/Contents/Home" > /dev/null); done
 (jenv rehash &) 2> /dev/null
+
+# vim-polygot bundle
+mkdir -p ~/.vim/pack/default/start && \
+    ln -s ~/.vim-polyglot /Users/yalinh/.vim/pack/default/start/vim-polyglot

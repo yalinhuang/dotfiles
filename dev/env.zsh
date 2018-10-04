11a0013a64c7c1add8984636a6634f47588c6bac
Ya-Lin Huang <yalinh@netflix.com>
2018-10-04 15:35:20 -0700

    Fancy prompt

--- dev/env.zsh
+++ dev/env.zsh
@@ -1,5 +1,10 @@
 export PATH=$PATH:$HOME/.cargo/bin
-export PROMPT='${ret_status} %{$fg[cyan]%}%30<...<%~%<<%{$reset_color%} $(git_prompt_info) '
+
+ret_status="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
+dir_path="%{$fg[cyan]%}%30<...<%~%<<"
+ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg[red]%}["
+ZSH_THEME_GIT_PROMPT_SUFFIX="]"
+export PROMPT='${ret_status}${dir_path}$(git_prompt_info)%{$reset_color%} '
 
 # By default, `zsh` prints '%' at the end of a partial line (a line not ending in '\n') to
 # indicate the end of line.
